80fde9f74a67dfdfe1aa1973ce3ac5f02a5690e1
dkorneva <dariakorneva20@gmail.com>
2026-07-31 15:19:56 +0300

    Lodash and Zod added

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -2,39 +2,45 @@
 // Он является посредником между сервером и клиентов, позволяющий им использовать один и тот же маршрутизатор (роутер) для обработки запросов HTTP
 
 import { initTRPC } from '@trpc/server'
+// lodash позволяет генерировать фейковый контент
+import _ from 'lodash'
+import { z } from 'zod'
 
-const ideas = [
-  {
-    nick: 'cool-idea-nick-1',
-    name: 'Idea 1',
-    description: 'Idea 1 description...',
-  },
-  {
-    nick: 'cool-idea-nick-2',
-    name: 'Idea 2',
-    description: 'Idea 2 description...',
-  },
-  {
-    nick: 'cool-idea-nick-3',
-    name: 'Idea 3',
-    description: 'Idea 3 description...',
-  },
-  {
-    nick: 'cool-idea-nick-4',
-    name: 'Idea 4',
-    description: 'Idea 4 description...',
-  },
-  {
-    nick: 'cool-idea-nick-5',
-    name: 'Idea 5',
-    description: 'Idea 5 description...',
-  },
-]
+// const ideas = [
+//   {
+//     nick: 'cool-idea-nick-1',
+//     name: 'Idea 1',
+//     description: 'Idea 1 description...',
+//   },
+//   {
+//     nick: 'cool-idea-nick-2',
+//     name: 'Idea 2',
+//     description: 'Idea 2 description...',
+//   },
+//   {
+//     nick: 'cool-idea-nick-3',
+//     name: 'Idea 3',
+//     description: 'Idea 3 description...',
+//   },
+//   {
+//     nick: 'cool-idea-nick-4',
+//     name: 'Idea 4',
+//     description: 'Idea 4 description...',
+//   },
+//   {
+//     nick: 'cool-idea-nick-5',
+//     name: 'Idea 5',
+//     description: 'Idea 5 description...',
+//   },
+// ]
 
-const x: string = 'hello'
-if (Math.random() + 1) {
-  console.info(x)
-}
+// круглые скобки () заменяют конструкцию с return
+const ideas = _.times(100, (i) => ({
+  nick: `cool-idea-nick-${i}`,
+  name: `Idea ${i}`,
+  description: `Description of idea ${i}...`,
+  text: _.times(100, (j) => `<p>Text paragraph ${j} of idea ${i}...</p>`).join(''),
+}))
 
 const trpc = initTRPC.create()
 
@@ -46,8 +52,23 @@ export const trpcRouter = trpc.router({
 
   // в query передаём ту функцию, которая должна быть вызвана, когда мы запросим её с фронта
   getIdeas: trpc.procedure.query(() => {
-    return { ideas }
+    return { ideas: ideas.map((idea) => _.pick(idea, ['nick', 'name', 'description'])) }
   }),
+  // задача: с фронта передавать на бэкенд ник идеи
+  // чтобы процедура принимала входные параметры, необходимо использовать input()
+  // в input передаётся zod схема
+  // zod позволяет одновременно валидировать и типизировать данные
+  // в input должен приходить объект, в котором хранится ideaNick, в котором хранится строка
+  getIdea: trpc.procedure
+    .input(
+      z.object({
+        ideaNick: z.string(),
+      })
+    )
+    .query(({ input }) => {
+      const idea = ideas.find((idea) => idea.nick === input.ideaNick)
+      return { idea: idea || null }
+    }),
 })
 
 // на бэкенде этот тип не нужен, он нужен на фронтенде, чтобы синхронизировать типы
