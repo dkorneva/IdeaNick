2b70e27a44dd8d5982a5b81a6abd1c0f8a76c3eb
dkorneva <dariakorneva20@gmail.com>
2026-08-04 18:57:07 +0300

    Styles for AllIdeasPage and ViewIdeaPage

--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom'
 import type { ViewIdeaRouteParams } from '../../lib/routes'
 import { trpc } from '../../lib/trpc'
+import css from './index.module.scss'
 
 export const ViewIdeaPage = () => {
   const { ideaNick } = useParams() as ViewIdeaRouteParams
@@ -17,16 +18,16 @@ export const ViewIdeaPage = () => {
     return <span>Error: {error.message}</span>
   }
 
-  if (!data.idea) {
+  if (!data?.idea) {
     return <span>Idea not found</span>
   }
 
   return (
     <div>
-      <h1>{data?.idea.name}</h1>
-      <p>{data?.idea.description}</p>
+      <h1 className={css.title}>{data?.idea.name}</h1>
+      <p className={css.description}>{data?.idea.description}</p>
 
-      <div dangerouslySetInnerHTML={{ __html: data!.idea.text }} />
+      <div className={css.text} dangerouslySetInnerHTML={{ __html: data!.idea.text }} />
     </div>
   )
 }
