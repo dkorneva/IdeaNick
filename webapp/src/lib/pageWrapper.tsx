cbd0acae9ab645b57107e26e367743b3fadbe420
dkorneva <dariakorneva20@gmail.com>
2026-08-12 16:27:27 +0300

    Add getAuthorizedMe function to pageWrapper and update EditProfilePage to use it

--- webapp/src/lib/pageWrapper.tsx
+++ webapp/src/lib/pageWrapper.tsx
@@ -22,6 +22,8 @@ const checkAccessFn = <T,>(value: T, message?: string): void => {
   }
 }
 
+class GetAuthorizedMeError extends Error {}
+
 type Props = Record<string, any>
 type QueryResult = UseTRPCQueryResult<any, any>
 type QuerySuccessResult<TQueryResult extends QueryResult> = UseTRPCQuerySuccessResult<
@@ -37,6 +39,7 @@ type HelperProps<TQueryResult extends QueryResult | undefined> = {
 type SetPropsProps<TQueryResult extends QueryResult | undefined> = HelperProps<TQueryResult> & {
   checkExists: typeof checkExistsFn
   checkAccess: typeof checkAccessFn
+  getAuthorizedMe: (message?: string) => NonNullable<AppContext['me']> | never
 }
 
 type PageWrapperProps<TProps extends Props, TQueryResult extends QueryResult | undefined> = {
@@ -114,8 +117,21 @@ const PageWrapper = <TProps extends Props = {}, TQueryResult extends QueryResult
       return <NotFoundPage title={checkExistsTitle} message={checkExistsMessage} />
     }
   }
+
+  const getAuthorizedMe = (message?: string) => {
+    if (!ctx.me) {
+      throw new GetAuthorizedMeError(message)
+    }
+    return ctx.me
+  }
+
   try {
-    const props = setProps?.({ ...helperProps, checkExists: checkExistsFn, checkAccess: checkAccessFn }) as TProps
+    const props = setProps?.({
+      ...helperProps,
+      checkExists: checkExistsFn,
+      checkAccess: checkAccessFn,
+      getAuthorizedMe,
+    }) as TProps
     return <Page {...props} />
   } catch (error) {
     if (error instanceof CheckExistsError) {
@@ -123,6 +139,9 @@ const PageWrapper = <TProps extends Props = {}, TQueryResult extends QueryResult
     }
     if (error instanceof CheckAccessError) {
       return <ErrorPageComponent title={checkAccessTitle} message={error.message || checkAccessMessage} />
+    }
+    if (error instanceof GetAuthorizedMeError) {
+      return <ErrorPageComponent title={authorizedOnlyTitle} message={error.message || authorizedOnlyMessage} />
     }
     throw error
   }
