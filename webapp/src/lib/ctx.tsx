75a36102539bc05326a4b4d03607418a7892bd7d
dkorneva <dariakorneva20@gmail.com>
2026-08-12 21:25:45 +0300

    Add auto infinite loading and loading state styles to button component

--- webapp/src/lib/ctx.tsx
+++ webapp/src/lib/ctx.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-restricted-imports */
 import type { TrpcRouterOutput } from '@IdeaNick/backend/src/router'
 import { createContext, useContext } from 'react'
+import { Loader } from '../components/Loader'
 import { trpc } from './trpc'
 
 export type AppContext = {
@@ -15,7 +16,7 @@ export const AppContextProvider = ({ children }: { children: React.ReactNode }) 
   const { data, error, isLoading, isFetching, isError } = trpc.getMe.useQuery()
   return (
     <AppReactContext.Provider value={{ me: data?.me || null }}>
-      {isLoading || isFetching ? <p>Loading...</p> : isError ? <p>Error: {error.message}</p> : children}
+      {isLoading || isFetching ? <Loader type="page" /> : isError ? <p>Error: {error.message}</p> : children}
     </AppReactContext.Provider>
   )
 }
--- webapp/src/pages/ideas/AllIdeasPage/index.tsx
+++ webapp/src/pages/ideas/AllIdeasPage/index.tsx
@@ -1,5 +1,8 @@
+import InfiniteScroll from 'react-infinite-scroller'
 import { Link } from 'react-router-dom'
 import { Alert } from '../../../components/Alert'
+import { layoutContentElRef } from '../../../components/Layout'
+import { Loader } from '../../../components/Loader'
 import { Segment } from '../../../components/Segment'
 import { getViewIdeaRoute } from '../../../lib/routes'
 import { trpc } from '../../../lib/trpc'
@@ -22,40 +25,48 @@ export const AllIdeasPage = () => {
   return (
     <Segment title="All Ideas">
       {isLoading || isRefetching ? (
-        <div>Loading...</div>
+        <Loader type="section" />
       ) : isError ? (
         <Alert color="red">{error.message}</Alert>
       ) : (
         <div className={css.ideas}>
-          {/* map - проходка по массиву, которая возвращает новый результирующий массив */}
-          {/* pages - массив из того, что возвращает endpoint */}
-          {data.pages
-            .flatMap((page) => page.ideas)
-            .map((idea) => (
-              <div className={css.idea} key={idea.nick}>
-                <Segment
-                  size={2}
-                  title={
-                    <Link className={css.ideaLink} to={getViewIdeaRoute({ ideaNick: idea.nick })}>
-                      {idea.name}
-                    </Link>
-                  }
-                  description={idea.description}
-                ></Segment>
+          <InfiniteScroll
+            // threshold - количество пикселей, которое нужно отмерить от конца списка
+            threshold={250}
+            loadMore={() => {
+              if (!isFetchingNextPage && hasNextPage) {
+                void fetchNextPage()
+              }
+            }}
+            hasMore={hasNextPage}
+            loader={
+              <div className={css.more} key="loader">
+                <Loader type="section" />
               </div>
-            ))}
-          <div className={css.more}>
-            {hasNextPage && !isFetchingNextPage && (
-              <button
-                onClick={() => {
-                  void fetchNextPage()
-                }}
-              >
-                Load more
-              </button>
-            )}
-            {isFetchingNextPage && <span>Loading...</span>}
-          </div>
+            }
+            // функция, которая должна вернуть объект, который скроллится и за скроллом которого надо следить
+            getScrollParent={() => layoutContentElRef.current}
+            // useWindow - будем следить за скроллом окна, а не родительского элемента данного блока
+            useWindow={(layoutContentElRef.current && getComputedStyle(layoutContentElRef.current).overflow) !== 'auto'}
+          >
+            {/* map - проходка по массиву, которая возвращает новый результирующий массив */}
+            {/* pages - массив из того, что возвращает endpoint */}
+            {data.pages
+              .flatMap((page) => page.ideas)
+              .map((idea) => (
+                <div className={css.idea} key={idea.nick}>
+                  <Segment
+                    size={2}
+                    title={
+                      <Link className={css.ideaLink} to={getViewIdeaRoute({ ideaNick: idea.nick })}>
+                        {idea.name}
+                      </Link>
+                    }
+                    description={idea.description}
+                  ></Segment>
+                </div>
+              ))}
+          </InfiniteScroll>
         </div>
       )}
     </Segment>
