49ff304bdef0fee8527b6908eb4eefda9a1d5d83
dkorneva <dariakorneva20@gmail.com>
2026-08-17 19:11:05 +0300

    Sentry sourcemap added for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -41,7 +41,7 @@ void (async () => {
     server = expressApp.listen(env.PORT, () => {
       logger.info('express', `Listening at http://localhost:${env.PORT}`)
     })
-    throw new Error('Unexpected error 2')
+    throw new Error('Error with sourcemap 2')
   } catch (error) {
     logger.error('app', error)
     server?.close()
