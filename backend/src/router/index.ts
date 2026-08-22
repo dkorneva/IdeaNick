0ddcdd96ee571bbe7abdd2c90553555649fd21e4
dkorneva <dariakorneva20@gmail.com>
2026-08-22 23:41:54 +0300

    Fix deploy workflow and auth route casing

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,8 +1,8 @@
 import { type inferRouterInputs, type inferRouterOutputs } from '@trpc/server'
 import { createTrpcRouter } from '../lib/trpc'
 // @index('./**/index.ts', f => `import { ${f.path.split('/').slice(0, -1).pop()}TrpcRoute } from '${f.path.split('/').slice(0, -1).join('/')}'`)
+import { signInTrpcRoute } from './auth/SignIn'
 import { getMeTrpcRoute } from './auth/getMe'
-import { signInTrpcRoute } from './auth/signIn'
 import { signUpTrpcRoute } from './auth/signUp'
 import { updatePasswordTrpcRoute } from './auth/updatePassword'
 import { updateProfileTrpcRoute } from './auth/updateProfile'
